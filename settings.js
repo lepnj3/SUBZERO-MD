13f56e76285d3191779b9363e50fe4576a0c00e3
lepnj3 <latranstyle@gmail.com>
2025-08-20 03:34:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   SESSION_ID: "SUBZERO-MD;;;yEdAjT7Y#hIz0KzBzkURxOlzO4zyhv0q16O-ZvZGthT96rbYWsxI", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  OWNER_NUMBER: "237678231229", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
