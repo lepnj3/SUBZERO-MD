a211342079c7466669e575f8b1b1ffef1bf4480a
lepnj3 <latranstyle@gmail.com>
2025-10-28 02:52:45 +0100

    Update settings.js for Tiflow12

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~405b6e2741ce0d413d60ec4b50a9a19b92999873", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "237678231229", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~ZuHxJk",
+  OWNER_NUMBER: "+237678231229", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
